6bb5447bce9c9900b28cdfdb379d30e1077a11a1
Dmytro Melnychuk <dmytromelnychuk@Dmytros-MacBook-Pro.local>
2023-08-30 23:23:11 +0300

    feat: change param's name for bucket on AWS

--- config/middlewares.js
+++ config/middlewares.js
@@ -15,9 +15,9 @@ module.exports = ({ env }) => [
             'lh3.googleusercontent.com', // google avatars
             'platform-lookaside.fbsbx.com', // facebook avatars
             'dl.airtable.com', // strapi marketplace
-            `https://${env('AWS_BUCKET')}.s3.amazonaws.com`
+            `https://${env('AWS_BUCKET_NAME')}.s3.amazonaws.com`
           ],
-          'media-src': ["'self'", 'data:', 'blob:', `https://${env('AWS_BUCKET')}.s3.amazonaws.com`],
+          'media-src': ["'self'", 'data:', 'blob:', `https://${env('AWS_BUCKET_NAME')}.s3.amazonaws.com`],
           upgradeInsecureRequests: null,
         },
       },
